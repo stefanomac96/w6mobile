a02fd7d55e011ad30b8ca247319bea899fd2edb9
Stefano Maccarinelli <118759138+stefanomac96@users.noreply.github.com>
2026-08-18 18:01:37 +0200

    First upload v0.2026-08-18A

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Stelex Software-W6 - Catch the Killer-0.2026-08-18A";
+const contentToCache = [
+    "Build/iOS (WebApp).loader.js",
+    "Build/iOS (WebApp).framework.js",
+    "Build/iOS (WebApp).data",
+    "Build/iOS (WebApp).wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
